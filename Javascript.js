cd43758fdf8f228073b7176a6929251ac98ff291
swordmaster2005 <soulmaster9794@gmail.com>
2023-03-17 00:50:58 +0530

    price of year

---
+++ Javascript.js
@@ -0,0 +1,19 @@
+const priveChange = document.querySelector('.checkbox')
+const basic= document.querySelector('.basic')
+const professional = document.querySelector('.professional-tit')
+const master = document.querySelector('.master')
+
+
+priveChange.addEventListener("change", (p)=>{
+    if(basic.textContent=="$ 19.99"){
+        basic.textContent="$ 199.99"
+        professional.textContent="$ 249.99"
+        master.textContent="$ 399.99"
+    }
+    else{
+        basic.textContent="$ 19.99"
+        professional.textContent="$ 24.99"
+        master.textContent="$ 39.99"
+    }
+
+})
